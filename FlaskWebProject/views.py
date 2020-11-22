cd1ba4aedf12cbf4cc9adf5e107250ad0f7f9501
vr772b <vr772b@att.com>
2020-11-21 21:11:16 -0500

    fixing typo5 in def build_mdsal_app indentation in views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -19,18 +19,18 @@ imageSourceUrl = 'https://'+ app.config['BLOB_ACCOUNT']  + '.blob.core.windows.n
 @app.route('/home')
 @login_required
 def home():
-    user = User.query.filter_by(username=current_user.username).first_or_404()
-    posts = Post.query.all()
     log = request.values.get('log_button')
     if log:
-     if log == 'info':
-         app.logger.info('No issue.')
-     elif log == 'warning':
-         app.logger.warning('Warning occurred.')
-     elif log == 'error':
-         app.logger.error('Error occurred.')
-     elif log == 'critical':
-         app.logger.critical('Critical error occurred.')
+        if log == 'info':
+            app.logger.info('No issue.')
+        elif log == 'warning':
+            app.logger.warning('Warning occurred.')
+        elif log == 'error':
+            app.logger.error('Error occurred.')
+        elif log == 'critical':
+            app.logger.critical('Critical error occurred.')
+    user = User.query.filter_by(username=current_user.username).first_or_404()
+    posts = Post.query.all()
     return render_template(
         'index.html',
         title='Home Page',
@@ -97,11 +97,7 @@ def authorized():
     if request.args.get('code'):
         cache = _load_cache()
         # TODO: Acquire a token from a built msal app, along with the appropriate redirect URI
-        result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
-          request.args['code'],
-          scopes=Config.SCOPE,
-          redirect_uri=url_for('authorized', _external=True, _scheme='https')
-          )
+        result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(request.args['code'],scopes=Config.SCOPE,redirect_uri=url_for('authorized', _external=True, _scheme='https'))
         if "error" in result:
             return render_template("auth_error.html", result=result)
         session["user"] = result.get("id_token_claims")
@@ -133,12 +129,10 @@ def _load_cache():
 def _save_cache(cache):
     # TODO: Save the cache, if it has changed
 
-    def _build_msal_app(cache=None, authority=None):
-        return msal.ConfidentialClientApplication(Config.CLIENT_ID, authority=authority or Config.AUTHORITY,client_credential=Config.CLIENT_SECRET,token_cache=cache)
+def _build_msal_app(cache=None, authority=None):
+    # TODO: Return a ConfidentialClientApplication
+    return msal.ConfidentialClientApplication(Config.CLIENT_ID, authority=authority or Config.AUTHORITY,client_credential=Config.CLIENT_SECRET,token_cache=cache)
 
 def _build_auth_url(authority=None, scopes=None, state=None):
     # TODO: Return the full Auth Request URL with appropriate Redirect URI
-    return _build_msal_app(authority=authority).get_authorization_request_url(
-    scopes or [],
-    state=state or str(uuid.uuid4()),
-    redirect_uri=url_for('authorized', _external=True, _scheme='https'))
+    return _build_msal_app(authority=authority).get_authorization_request_url(scopes or [],state=state or str(uuid.uuid4()),redirect_uri=url_for('authorized', _external=True, _scheme='https'))
