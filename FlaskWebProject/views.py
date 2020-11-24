d1b4016ac68fb0170070b7e7113ea2c10755e9e6
veerendradr <60857842+veerendradr@users.noreply.github.com>
2020-11-24 17:22:14 -0500

    updated load cache details to while using msal

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -125,7 +125,9 @@ def logout():
 
 def _load_cache():
     # TODO: Load the cache from `msal`, if it exists
-    cache = None
+    cache = msal.SerializableTokenCache()
+    if session.get("token_cache"):
+        cache.deserialize(session["token_cache"])
     return cache
 
 def _save_cache(cache):
